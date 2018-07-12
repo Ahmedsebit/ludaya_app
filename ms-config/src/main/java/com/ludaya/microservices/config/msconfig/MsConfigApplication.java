694da405fc471077e20ab6e69da65e0075cbd9d0
Ahmed.sebit <ahmed.yusuf@andela.com>
2018-07-12 14:39:09 +0300

    ADD config server

---
+++ ms-config/src/main/java/com/ludaya/microservices/config/msconfig/MsConfigApplication.java
@@ -0,0 +1,14 @@
+package com.ludaya.microservices.config.msconfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class MsConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsConfigApplication.class, args);
+	}
+}
